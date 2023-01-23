077b6abe90b9d6894e9591c31dcf18ae812b965a
Nathan Zeager <nathanzeager@hotmail.com>
2023-01-23 12:02:12 -0500

    Set up axios request to search movies by title

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+// import { useState, useEffect } from 'react';
+
 
 function App() {
+
+    axios.get('https://api.themoviedb.org/3/search/movie?api_key=23ca29d10fcb0ee407a6a0fddb586cfc&language=en-US&page=1&include_adult=false&query=signs')
+    .then(response => {
+      console.log(response.data);
+    })
+    .catch(error => {
+      console.log(error);
+    });
+
   return (
     <div className="App">
       <header className="App-header">
